ff3a104a1eaf3f52ede51f070e02d42e72c7722f
Zczxl428 <88660870+Zczxl428@users.noreply.github.com>
2023-02-12 12:24:06 +0800

    Update basic-layout.vue

--- docs/examples/layout/basic-layout.vue
+++ docs/examples/layout/basic-layout.vue
@@ -2,7 +2,7 @@
   <el-row>
     <el-col :span="24"><div class="grid-content ep-bg-purple-dark" /></el-col>
   </el-row>
-  <el-row>
+  <el-row gutter="20">
     <el-col :span="12"><div class="grid-content ep-bg-purple" /></el-col>
     <el-col :span="12"><div class="grid-content ep-bg-purple-light" /></el-col>
   </el-row>
